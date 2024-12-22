33fb30bafd81061c750bb57bd54af09f696d608c
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-12-22 21:41:47 +0530

    Update gps_.py

--- Merisa/modules/gps_.py
+++ Merisa/modules/gps_.py
@@ -1,27 +1,3 @@
-"""MIT License
-
-Copyright (c) 2023-24 Noob-QuantamBot
-
-          GITHUB: NOOB-MUKESH
-          TELEGRAM: @MR_SUKKUN
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE."""
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
 import asyncio
@@ -69,10 +45,6 @@ async def distance(bot, message):
     y = message.text.split(" ")[1].split(',',4)[2:4]
     try:
 
-        """
-        ---------github :-NOOB-MUKESH -----
-        ---------telegram : @itz_legend_coder-----
-        """
         distance=(great_circle(x,y).miles)
 
         await message.reply_text(f"Total distance between {x[0]},{x[1]} and {y[0]},{y[1]} is {distance}")
@@ -84,8 +56,8 @@ async def distance(bot, message):
 __HELP__ = """
 sᴇɴᴅs ʏᴏᴜ ᴛʜᴇ ɢᴘs ʟᴏᴄᴀᴛɪᴏɴ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ǫᴜᴇʀʏ...
 
- ❍ /gps <ʟᴏᴄᴀᴛɪᴏɴ>*:* ɢᴇᴛ ɢᴘs ʟᴏᴄᴀᴛɪᴏɴ.
- ❍ /distance  to measure distance 
+ ๏ /gps <ʟᴏᴄᴀᴛɪᴏɴ>*:* ɢᴇᴛ ɢᴘs ʟᴏᴄᴀᴛɪᴏɴ.
+ ๏ /distance  to measure distance 
 """
 
 __MODULE__ = "Gᴘs"
