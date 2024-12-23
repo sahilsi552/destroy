7af8e113d13139f626ff6d9a21ee1011d35bfbe6
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-12-23 10:45:49 +0530

    d

--- Merisa/modules/stats.py
+++ Merisa/modules/stats.py
@@ -1,1 +1,16 @@
 
+# -----------CREDITS -----------
+# telegram : @legend_coder
+# github : noob-mukesh
+from pyrogram import filters
+import asyncio
+from .. import QuantamBot as Mukesh
+from Merisa.utils.button_help import *
+from Merisa.database import *
+
+
+@Mukesh.on_message(filters.command(["stats", f"stats@{Mukesh.username}"]))
+async def stats(_, m):
+    users = len(await get_served_users())
+    chats = len(await get_served_chats())
+    await m.reply(f"""  ᴛᴏᴛᴀʟ ꜱᴛᴀᴛꜱ  {Mukesh.mention}:\nᴜꜱᴇʀꜱ: {users}\nᴄʜᴀᴛ: {chats}""")
