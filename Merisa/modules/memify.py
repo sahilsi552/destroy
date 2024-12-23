428a4a3065e16cb1e49c51470ec3dc49a58f5038
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-12-23 09:32:54 +0530

    added

---
+++ Merisa/modules/memify.py
@@ -0,0 +1,164 @@
+import os
+import textwrap
+from PIL import Image, ImageDraw, ImageFont
+from pyrogram import Client, filters
+from Merisa import QuantamBot as app
+@app.on_message(filters.command("mmf") & filters.reply)
+async def handler(client, message):
+    if not message.reply_to_message:
+        await message.reply("Provide Some Text To Draw! 🖊️")
+        return
+
+    reply_message = message.reply_to_message
+
+    if not reply_message.media:
+        await message.reply("Reply to an image/sticker. 🖼️")
+        return
+
+    file_path = await client.download_media(reply_message)
+
+    msg = await message.reply("Memifying this image! ✊🏻")
+
+    text = message.text.split(" ", 1)[1].strip() if len(message.text.split(" ", 1)) > 1 else ""
+
+    if len(text) < 1:
+        await msg.reply("You might want to try /mmf text")
+        return
+
+    meme_path = await drawText(file_path, text)
+
+    await client.send_document(message.chat.id, document=meme_path)
+
+    await msg.delete()
+    os.remove(meme_path)
+    
+async def drawText(image_path, text):
+    
+    img = Image.open(image_path)
+    os.remove(image_path)
+    i_width, i_height = img.size
+    if os.name == "nt":
+        fnt = "ariel.ttf"
+    else:
+        fnt = "Merisa/utils/fonts_.otf"
+
+    m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
+
+    if ";" in text:
+        upper_text, lower_text = text.split(";")
+
+    else:
+        upper_text = text
+
+        lower_text = ""
+
+    draw = ImageDraw.Draw(img)
+
+    current_h, pad = 10, 5
+
+    if upper_text:
+        for u_text in textwrap.wrap(upper_text, width=15):
+            uwl, uht, uwr, uhb = m_font.getbbox(u_text)
+            u_width, u_height = uwr - uwl, uhb - uht
+
+            draw.text(
+                xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=(((i_width - u_width) / 2) + 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 2),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 2),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+
+            current_h += u_height + pad
+
+    if lower_text:
+        for l_text in textwrap.wrap(lower_text, width=15):
+            uwl, uht, uwr, uhb = m_font.getbbox(l_text)
+            u_width, u_height = uwr - uwl, uhb - uht
+
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) - 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) + 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) - 2,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) + 2,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+
+            current_h += u_height + pad
+
+    image_name = "memify.webp"
+
+    webp_file = os.path.join(image_name)
+
+    img.save(webp_file, "webp")
+
+    return webp_file
